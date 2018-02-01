bede9a1080d10de1ffa70e64dd04e252934c58c3
Guillaume Piot <guillaume@cotidia.com>
2018-02-01 16:31:09 +0000

    Add app_name to urls.

--- socialshare/urls.py
+++ socialshare/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from socialshare.views import share_email
 
+app_name = 'socialshare'
 
 urlpatterns = [
     url(r'^send-friend/$', share_email, name='social-share-email'),
